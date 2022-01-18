35116603f2a5340ee5e3f3663a79a716a4c3f8ff
James Mitchell <JamesMitchell@Rocketmortgage.com>
2022-01-18 16:41:50 -0500

    second commit

--- src/app/edit-profile/edit-profile.component.ts
+++ src/app/edit-profile/edit-profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileServiceService } from '../profile-service.service';
 import { User } from '../user';
+
 @Component({
   selector: 'app-edit-profile',
   templateUrl: './edit-profile.component.html',
